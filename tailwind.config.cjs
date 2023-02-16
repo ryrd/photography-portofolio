65310320890fae217e210b6ae7a7d90f29064119
ryrd <rdridhani@gmail.com>
2023-02-16 18:33:05 +0800

    fix credit text hover on different size

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,18 @@ module.exports = {
     fontFamily: {
       'orbitron': ['Orbitron', 'sans-serif'],
     },
-    extend: {},
+    // screens: {
+    //   'sm': '640px',
+    //   'md': '768px',
+    //   'lg': '1024px',
+    //   'xl': '1280px',
+    //   '2xl': '1536px',
+    // },
+    extend: {
+      screens: {
+        '3xl': '2100px',
+      },
+    },
   },
   plugins: [],
 }
