3206bf13e3238a7a54474b629d873a82ff148ca0
ryrd <rdridhani@gmail.com>
2023-03-04 07:04:22 +0800

    fix other photo click view bug

--- src/components/three.vue
+++ src/components/three.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted, defineEmits } from 'vue';
-import { Color as TColor, AmbientLight, TextureLoader, Raycaster, PlaneGeometry, BoxGeometry, DirectionalLight, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, Vector2, WebGLRenderer } from 'three'
+import { Color as TColor, AmbientLight, TextureLoader, Raycaster, PlaneGeometry, BoxGeometry, DirectionalLight, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, Vector2, WebGLRenderer, Intersection, Object3D, Event } from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import {Reflector} from 'three/examples/jsm/objects/Reflector'
 import gsap, {Expo} from 'gsap'
@@ -9,12 +9,6 @@ import data from '../data'
 const canvasRef = ref()
 
 let renderer: WebGLRenderer, composer: any
-let scrollable = true
-const prevCoordinate = {
-  X : 0,
-  Y : 0,
-  Z : 0,
-}
 
 const pointer = new Vector2()
 const raycaster = new Raycaster()
@@ -97,8 +91,15 @@ function animate(){
   requestAnimationFrame(animate)
 }
 
-let portrait: boolean = false
-if (window.matchMedia("(orientation: portrait)").matches) portrait = true
+let portrait = window.matchMedia("(orientation: portrait)").matches ? true : false,
+    scrollable = true,
+    aPhotoIsViewed = false,
+    viewedPhotoId = -1
+const prevCoordinate = {
+  X : 0,
+  Y : 0,
+  Z : 0,
+}
 
 onMounted(() => {
   renderer = new WebGLRenderer({canvas: canvasRef.value, antialias: true })
@@ -124,6 +125,25 @@ onMounted(() => {
     camera.rotation.set(mousePosY, mousePosX, 0)
   })
 
+  function closePhotoPreview(intersect: Intersection<Object3D<Event>>){
+    document.body.style.overflowY = 'scroll'
+  
+    gsap.to(intersect.object.position, {
+      duration: .2,
+      ease: Expo.easeIn,
+      x : prevCoordinate.X,
+      y : prevCoordinate.Y,
+      z : prevCoordinate.Z,
+    })
+    prevCoordinate.X = 0; 
+    prevCoordinate.Y = 0; 
+    prevCoordinate.Z = 0;
+
+    scrollable = true
+    aPhotoIsViewed = false
+    viewedPhotoId = -1
+  }
+
   let clicked = 0, lastClick = 0
   window.addEventListener("mousedown", e => {
     pointer.x = (e.clientX/window.innerWidth)*2-1
@@ -132,10 +152,14 @@ onMounted(() => {
     raycaster.setFromCamera(pointer, camera)
     const intersect = raycaster.intersectObjects(scene.children)[0] || null
 
-    if(intersect !== null){
-      if (intersect.object.name === "photo" ) {
-        scrollable = !scrollable
-  
+    if (intersect === null) return
+    
+    if (intersect.object.name === "photo") {
+      if (viewedPhotoId === -1){
+        scrollable = false
+        aPhotoIsViewed = true
+        viewedPhotoId = intersect.object.id
+        
         if (!scrollable) {
           prevCoordinate.X = intersect.object.position.x
           prevCoordinate.Y = intersect.object.position.y
@@ -151,97 +175,86 @@ onMounted(() => {
             z : portrait ? ((document.body.getBoundingClientRect().top)*-0.007)-.4 : ((document.body.getBoundingClientRect().top)*-0.007)-.18,
           })
         }
-        else{
-          document.body.style.overflowY = 'scroll'
-  
-          gsap.to(intersect.object.position, {
-            duration: .2,
-            ease: Expo.easeIn,
-            x : prevCoordinate.X,
-            y : prevCoordinate.Y,
-            z : prevCoordinate.Z,
-          })
-          prevCoordinate.X = 0; 
-          prevCoordinate.Y = 0; 
-          prevCoordinate.Z = 0;
-        }
       }
-      else if(intersect.object.name === "Hoodie" || intersect.object.name === "pant" || intersect.object.name === "man" || intersect.object.name === "leftshoe"){
-        let date = new Date()
-        let time = date.getTime()
-        const time_between_taps = 200
-        if (time-lastClick < time_between_taps) clicked += 1
-        lastClick = time
+      else if (intersect.object.id === viewedPhotoId){
+        closePhotoPreview(intersect)
+      }
+    }
+    else if(intersect.object.name === "Hoodie" || intersect.object.name === "pant" || intersect.object.name === "man" || intersect.object.name === "leftshoe"){
+      let date = new Date()
+      let time = date.getTime()
+      const time_between_taps = 200
+      if (time-lastClick < time_between_taps) clicked += 1
+      lastClick = time
+      
+      setTimeout(() => {
+        if(time-lastClick === 0) clicked = 0
+      }, 800)
+
+      if(clicked === 30){
+        const down = document.querySelector('#down')! as HTMLDivElement
+        down.style.height = '1200vh'
+
+        const loader2 = new GLTFLoader()
+        loader2.load('/man.gltf', (gltf: any) => {
+            const obj = gltf.scene
+            obj.position.set(-1.1,-.5,30)
+            obj.rotation.set(0, .3, 0)
+
+            scene.add(gltf.scene)
+        })
         
-        setTimeout(() => {
-          if(time-lastClick === 0) clicked = 0
-        }, 800)
-
-        if(clicked === 30){
-          const down = document.querySelector('#down')! as HTMLDivElement
-          down.style.height = '1200vh'
-
-          const loader2 = new GLTFLoader()
-          loader2.load('/man.gltf', (gltf: any) => {
-              const obj = gltf.scene
-              obj.position.set(-1.1,-.5,30)
-              obj.rotation.set(0, .3, 0)
-
-              scene.add(gltf.scene)
-          })
-          
-          const imgs = [
-            {
-              url: 'QdCRXF2F/m1.jpg',
-              dimension: {
-                width: .26,
-                height: .15,
-              },
-              position: {
-                x: -.7,
-                y: 1,
-                z: 30
-              }
+        const imgs = [
+          {
+            url: 'QdCRXF2F/m1.jpg',
+            dimension: {
+              width: .26,
+              height: .15,
             },
-            {
-              url: 'FF25vf2P/m2.jpg',
-              dimension: {
-                width: .24,
-                height: .12,
-              },
-              position: {
-                x: .9,
-                y: .2,
-                z: 40
-              }
+            position: {
+              x: -.7,
+              y: 1,
+              z: 30
+            }
+          },
+          {
+            url: 'FF25vf2P/m2.jpg',
+            dimension: {
+              width: .24,
+              height: .12,
             },
-            {
-              url: '506Td1pd/m3.jpg',
-              dimension: {
-                width: .24,
-                height: .12,
-              },
-              position: {
-                x: -.45,
-                y: .15,
-                z: 50
-              }
+            position: {
+              x: .9,
+              y: .2,
+              z: 40
+            }
+          },
+          {
+            url: '506Td1pd/m3.jpg',
+            dimension: {
+              width: .24,
+              height: .12,
             },
-          ]
+            position: {
+              x: -.45,
+              y: .15,
+              z: 50
+            }
+          },
+        ]
 
-          imgs.forEach(img => {
-            const eg = new TextureLoader().load(`https://i.postimg.cc/${img.url}`)
-            const egGeo = new PlaneGeometry(img.dimension.width, img.dimension.height)
-            const egMat = new MeshBasicMaterial({map: eg})
-            const egPhotoBox = new Mesh(egGeo, egMat)
-            egPhotoBox.position.set(img.position.x, img.position.y, img.position.z)
-            egPhotoBox.name = "photo"
-            scene.add(egPhotoBox)
-          })
-        }
+        imgs.forEach(img => {
+          const eg = new TextureLoader().load(`https://i.postimg.cc/${img.url}`)
+          const egGeo = new PlaneGeometry(img.dimension.width, img.dimension.height)
+          const egMat = new MeshBasicMaterial({map: eg})
+          const egPhotoBox = new Mesh(egGeo, egMat)
+          egPhotoBox.position.set(img.position.x, img.position.y, img.position.z)
+          egPhotoBox.name = "photo"
+          scene.add(egPhotoBox)
+        })
       }
+    }
 
-    };
   })
   
   animate()
